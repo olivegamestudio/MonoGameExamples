0d3e0b055c4b4caadf4b13120f3087c9bd0f5080
Simon Jefferies <simon.jefferies@inseinc.com>
2024-06-05 22:02:27 +0100

    Opening a file on android.

---
+++ OpeningFile/OpeningFile.Android/Game1.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+using System.IO;
+using Android.App;
+using Java.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace OpeningFile;
+public class Game1 : Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+
+    public Game1()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        /*using (Stream s = Application.Context.Assets.Open("TextFile1.txt"))
+        {
+            using (StreamReader r = new(s))
+            {
+                Debug.WriteLine(r.ReadToEnd());
+            }
+        }*/
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        // TODO: use this.Content to load your game content here
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        // TODO: Add your update logic here
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        // TODO: Add your drawing code here
+
+        base.Draw(gameTime);
+    }
+}
